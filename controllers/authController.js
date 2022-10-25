dde79025ef32448de76bbd19d1860190dbb9e742
Havva Zeynep Akdemir <83979496+Havvazeynep@users.noreply.github.com>
2022-10-25 23:35:21 +0300

    CodeRefactoring in React,Node.js, Express, MongoDB

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,27 +1,26 @@
 const User = require('../models/User')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-const asyncHandler = require('express-async-handler')
 
 // @desc Login
 // @route POST /auth
 // @access Public
-const login = asyncHandler(async (req, res) => {
+const login = async (req, res) => {
     const { username, password } = req.body
 
     if (!username || !password) {
-        return res.status(400).json({ message: 'All fields are requires' })
+        return res.status(400).json({ message: 'All fields are required' })
     }
 
     const foundUser = await User.findOne({ username }).exec()
 
-    if(!foundUser || !foundUser.active) {
+    if (!foundUser || !foundUser.active) {
         return res.status(401).json({ message: 'Unauthorized' })
     }
 
     const match = await bcrypt.compare(password, foundUser.password)
 
-    if(!match) return res.status(401).json({ message: 'Unauthorized' })
+    if (!match) return res.status(401).json({ message: 'Unauthorized' })
 
     const accessToken = jwt.sign(
         {
@@ -40,17 +39,17 @@ const login = asyncHandler(async (req, res) => {
         { expiresIn: '7d' }
     )
 
-    // Create secure cookie with refresh token
+    // Create secure cookie with refresh token 
     res.cookie('jwt', refreshToken, {
-        httpOnly: true, // accessible only by web server
+        httpOnly: true, //accessible only by web server 
         secure: true, //https
-        sameSite: 'None', //cross-site cookie
+        sameSite: 'None', //cross-site cookie 
         maxAge: 7 * 24 * 60 * 60 * 1000 //cookie expiry: set to match rT
     })
 
-    // Send accessToken containing username and roles
+    // Send accessToken containing username and roles 
     res.json({ accessToken })
-})
+}
 
 // @desc Refresh
 // @route GET /auth/refresh
@@ -65,10 +64,10 @@ const refresh = (req, res) => {
     jwt.verify(
         refreshToken,
         process.env.REFRESH_TOKEN_SECRET,
-        asyncHandler(async (err, decoded) => {
+        async (err, decoded) => {
             if (err) return res.status(403).json({ message: 'Forbidden' })
 
-            const foundUser = await User.findOne({ username: decoded.username}).exec()
+            const foundUser = await User.findOne({ username: decoded.username }).exec()
 
             if (!foundUser) return res.status(401).json({ message: 'Unauthorized' })
 
@@ -84,7 +83,7 @@ const refresh = (req, res) => {
             )
 
             res.json({ accessToken })
-        })
+        }
     )
 }
 
@@ -98,4 +97,8 @@ const logout = (req, res) => {
     res.json({ message: 'Cookie cleared' })
 }
 
-module.exports = { login, refresh, logout }
+module.exports = {
+    login,
+    refresh,
+    logout
+}
--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 require('dotenv').config()
+require('express-async-errors')
 const express = require('express')
 const app = express()
 const path = require('path')
-const {logger} = require('./middleware/logger')
+const { logger, logEvents } = require('./middleware/logger')
 const errorHandler = require('./middleware/errorHandler')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const corsOptions = require('./config/corsOptions')
 const connectDB = require('./config/dbConn')
 const mongoose = require('mongoose')
-const {logEvents} = require('./middleware/logger')
 const PORT = process.env.PORT || 3500
 
 console.log(process.env.NODE_ENV)
@@ -24,7 +24,7 @@ app.use(express.json())
 
 app.use(cookieParser())
 
-app.use('/', express.static(path.join(__dirname, '/public')))
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.use('/', require('./routes/root'))
 app.use('/auth', require('./routes/authRoutes'))
@@ -36,7 +36,7 @@ app.all('*', (req, res) => {
     if (req.accepts('html')) {
         res.sendFile(path.join(__dirname, 'views', '404.html'))
     } else if (req.accepts('json')) {
-        res.json({message: '404 Not Found'})
+        res.json({ message: '404 Not Found' })
     } else {
         res.type('txt').send('404 Not Found')
     }
@@ -45,11 +45,11 @@ app.all('*', (req, res) => {
 app.use(errorHandler)
 
 mongoose.connection.once('open', () => {
-    console.log('Connect to MangoDB')
+    console.log('Connected to MongoDB')
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 })
 
 mongoose.connection.on('error', err => {
     console.log(err)
-    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,'mongoErrLog.log')
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
 })
